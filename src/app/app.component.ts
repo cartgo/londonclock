62b13bedc4893852c54a20be1dea1063c9f84b03
cartgo <43975440+cartgo@users.noreply.github.com>
2020-09-20 23:37:31 -0400

    wknd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,7 @@ import { Component, Input , OnInit} from '@angular/core';
 })
 export class AppComponent implements OnInit{
   title = 'londonclock';
-  Title = 'London Clock';
-  // now: Date;
+  Title = 'London Clock'; 
   hours:number;
   minutes:number;
   seconds:number;
@@ -28,10 +27,9 @@ export class AppComponent implements OnInit{
     this.minutes = now.getMinutes();
     this.seconds = now.getSeconds();
     this.am = true;
-    if (this.hours >=12){this.am = false;
-      if(this.hours>12){this.hours -=12;}}else{
-        this.am = true;
-    }
+    if (this.hours >=12){this.am = !this.am;
+      this.hours = 0;}
+   
 
     setInterval(() => {
       if(this.pause ==false){
@@ -64,13 +62,21 @@ export class AppComponent implements OnInit{
         this.hours++;
       }
       if (this.hours >=12){
+        this.hours -=12;
           this.am = !this.am;
     }
   }
 
 
-    k(event){
+    k(event,inputhh?,inputmm?,inputss?){
       if(event.keyCode  ===13 ){
+        if (this.htf == false) {this.hours  = Math.round(inputhh.value)}
+        if (this.mtf == false) {this.minutes = Math.round(inputmm.value);}
+        if (this.stf == false) {this.seconds = Math.round(inputss.value);};
+
+        if(inputhh.value >12|| inputhh.value < 0 ){this.hours = 0;alert("please enter number between 0 - 12")}
+        if(inputmm.value >60|| inputmm.value < 0 ){this.minutes = 0;alert("please enter number between 0 - 60")}
+        if(inputss.value >60|| inputss.value < 0 ){this.seconds = 0;alert("please enter number between 0 - 60")}
         this.pause = false;
         this.htf = true;
         this.mtf = true;
